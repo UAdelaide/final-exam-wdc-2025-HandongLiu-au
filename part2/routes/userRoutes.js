fdb0e984dab85d9d0b00c09c680232d7e049fef4
Handong Liu <a1936695@adelaide.edu.au>
2025-06-20 08:03:19 +0000

    [automatic commit] 2025-06-20 08:03:19 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -13,19 +13,7 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/mydogs', async (req, res) => {
-  const user = req.session.user;
-  if (!user || user.role !== 'owner') {
-    return res.status(401).json({ error: 'Not authorized' });
-  }
 
-  try {
-    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [user.user_id]);
-    res.json(rows);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to load dogs' });
-  }
-});
 
 // POST a new user (simple signup)
 router.post('/register', async (req, res) => {
